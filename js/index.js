28efaf9e2933219eefa9745d0745f2c6486a81b3
Eva Reitzel <evareitzel@gmail.com>
2022-07-31 23:53:44 -0400

    Need local host 3000 to load

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {});
+// document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", getBooks());
+
+
+function getBooks(){
+  fetch('http://localhost:3000/books')
+  // .then((resp) => console.log(resp));
+  .then(resp => resp.json())
+  .then(data => console.log(data))
+  console.log('Here we are!')
+}
